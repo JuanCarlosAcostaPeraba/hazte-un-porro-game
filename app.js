4d1b333d661c2047a853f9bbf79f1c4052335f9a
Nuhacet Travieso del Rosario <nuhacet66@gmail.com>
2023-11-15 09:10:50 +0000

    Add user score to ranking and save to LocalStorage

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-// document.getElementById("1").addEventListener("click", () => {
-// 	console.log("1")
-// })
-console.log("1")
+// Obtener la puntuación actual del usuario
+const userScore = 100;
+
+// Obtener el ranking actual del LocalStorage
+let ranking = JSON.parse(localStorage.getItem('ranking')) || [];
+
+// Agregar la puntuación actual del usuario al ranking
+ranking.push(userScore);
+
+// Ordenar el ranking de mayor a menor
+ranking.sort((a, b) => b - a);
+
+// Guardar el ranking actualizado en el LocalStorage
+localStorage.setItem('ranking', JSON.stringify(ranking));
